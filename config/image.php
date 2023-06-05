1fca3b5fe72b82c68085057ee8084657b6abc30f
Christian Leo-Pernold <christian.leo@akwien.at>
2023-06-05 18:18:12 +0200

    Add Babler.

--- config/image.php
+++ config/image.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'url' => 'https://heute-at-prod-images.imgix.net/2023/06/03/51ddf4a6-42b0-4cbc-8400-57534dbff1e9.jpeg?rect=358%2C345%2C3200%2C1800&auto=format,compress&w=558',
-    'copy' => 'Sabine Hertel',
+    'url' => 'https://images.oe24.at/babler.jpg/1600x900Crop/542.874.396',
+    'copy' => 'APA',
 ];
