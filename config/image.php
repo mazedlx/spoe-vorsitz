ea3f6a6c029f0801e6b0ebe9eda9aa9566be0eb0
Christian Leo-Pernold <christian.leo@akwien.at>
2023-06-05 18:10:36 +0200

    Add Babler.

--- config/image.php
+++ config/image.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'url' => 'https://live.staticflickr.com/65535/52947023867_344130a0b9_k.jpg',
-    'copy' => 'SPÖ/David Višnjić',
+    'url' => 'https://heute-at-prod-images.imgix.net/2023/06/03/51ddf4a6-42b0-4cbc-8400-57534dbff1e9.jpeg?rect=358%2C345%2C3200%2C1800&auto=format,compress&w=558',
+    'copy' => 'Sabine Hertel',
 ];
